c60c917280dd7e19650dd88305ea31c469635e12
challadevops1 <124413798+challadevops1@users.noreply.github.com>
2024-03-27 20:37:49 +0530

    Create main.tf

---
+++ terraform-files/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "New-test-server" {
+  ami           = "ami-0c101f26f147fa7fd" 
+  instance_type = "t2.micro" 
+  key_name = "project"
+  vpc_security_group_ids= ["sg-020a949c964fb36e9"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("./learnawskey.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "New-test-server"
+  }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.New-test-server.public_ip} > inventory"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/terraform-files/ansibleplaybook.yml"
+  }
+  }
